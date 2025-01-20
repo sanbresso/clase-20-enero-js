e9dbf7e3287b85bbb4ccb922ff4bbe9307fdc9be
Sofia Bresso <sofibress@mail.com>
2025-01-20 19:37:31 -0300

    practicando variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let edad = 24;
+const nombre = "Sofía";
+const mayorDeEdad = true;
+
+console.log(nombre, "tiene", edad, "y es mayor de edad");
+console.log(mayorDeEdad);
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof mayorDeEdad);
